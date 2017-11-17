2047377b7fa5cb1952371d694a9c590e92f88c94
Jen Thakar <jathak@gmail.com>
2017-11-17 13:45:57 -0800

    Add serialization to FlagTrace

--- lib/src/extra/flag_trace.dart
+++ lib/src/extra/flag_trace.dart
@@ -10,12 +10,6 @@ import 'package:cs61a_scheme/cs61a_scheme.dart';
 
 import 'diagramming.dart';
 
-class FlagTrace {
-  List<FlagStep> steps = [];
-  String code;
-  FlagTrace(this.code);
-}
-
 class FlagTraceBuilder {
   FlagTrace trace;
   FlagTraceBuilder(List<Expression> code, Frame env) {
@@ -88,16 +82,62 @@ class FlagTraceBuilder {
   }
 }
 
-class FlagStep {
-  Diagram diagram;
-  Flag flag;
-  FlagStep(this.diagram, this.flag);
+class FlagTrace {
+  List<FlagStep> steps = [];
+  String code;
+  String language;
+  FlagTrace(this.code, [this.language='scheme']);
+
+  Map serialize() => {
+    'type': 'FlagTrace',
+    'code': code,
+    'language': language,
+    'steps': steps.map((s) => s.serialize()).toList()
+  };
+
+  FlagTrace deserialize(Map data) {
+    var trace = new FlagTrace(data['code'], data['language']);
+    trace.steps = data['steps'].map(Serialization.deserialize).toList();
+    return trace;
+  }
 }
 
-class Flag {
+class FlagStep extends SelfEvaluating with Serializable<FlagStep> {
+  Diagram diagram;
+  List<Flag> flags;
+  FlagStep(this.diagram, this.flags);
+
+  Map serialize() => {
+    'type': 'FlagStep',
+    'diagram': diagram.serialize(),
+    'flags': flags.map((f) => f.serialize()).toList()
+  };
+
+  FlagStep deserialize(Map data) => new FlagStep(
+    Serialization.deserialize(data['diagram']),
+    data['flags'].map(Serialization.deserialize).toList()
+  );
+}
+
+class Flag extends SelfEvaluating with Serializable<Flag> {
   String callExpression;
   List<Flag> operands = [];
   int frameId;
   List<Flag> body = [];
   Flag(this.callExpression, this.frameId);
+
+  Map serialize() => {
+    'type': 'Flag',
+    'callExpression': callExpression,
+    'operands': operands.map((f) => f.serialize()).toList(),
+    'frameId': frameId,
+    'body': body.map((f) => f.serialize()).toList()
+  };
+
+  Flag deserialize(Map data) {
+    var flag = new Flag(data['callExpression'], data['frameId']);
+    flag.operands = data['operands'].map(Serialization.deserialize).toList();
+    flag.body = data['body'].map(Serialization.deserialize).toList();
+    return flag;
+  }
 }
