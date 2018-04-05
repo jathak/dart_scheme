1b2fbda3c171505b6d24df96270bc5e23eeff256
Jen Thakar <jathak@gmail.com>
2018-04-05 12:41:31 -0700

    Add line editing to tool/repl.dart

--- tool/repl.dart
+++ tool/repl.dart
@@ -1,23 +1,38 @@
 import 'dart:io';
-import 'dart:convert';
+
+import 'package:cli_repl/cli_repl.dart';
 
 import 'package:cs61a_scheme/cs61a_scheme_extra.dart';
 import 'package:cs61a_scheme_impl/impl.dart' show StaffProjectImplementation;
 
 main() async {
   Interpreter interpreter = new Interpreter(new StaffProjectImplementation());
-  var lines = stdin.transform(UTF8.decoder).transform(new LineSplitter());
-  interpreter.logger = (e, newline) => newline ? print(e) : stdout.write(e);
-  interpreter.onExit = () => exit(0);
   interpreter.importLibrary(new ExtraLibrary());
   interpreter.importLibrary(new LogicLibrary());
+  interpreter.logger = (e, newline) => newline ? print(e) : stdout.write(e);
+  interpreter.onExit = () => exit(0);
   addPrimitive(interpreter.globalEnv, const SchemeSymbol("tco"), (e, env) {
     interpreter.tailCallOptimized = e.first.isTruthy;
     return undefined;
   }, 1);
-  stdout.write("scm> ");
-  await for (String line in lines) {
-    interpreter.run(line);
-    stdout.write("scm> ");
+  var repl = new Repl(prompt: 'scm> ', validator: matchingParens);
+  await for (var expr in repl.run()) {
+    interpreter.run(expr);
   }
+}
+
+bool matchingParens(String text) {
+  var tokens;
+  try {
+    tokens = tokenizeLines(text.split('\n')).toList();
+  } on FormatException {
+    return true;
+  }
+  int left = tokens.fold(0, (val, token) {
+    return val + (token == const SchemeSymbol('(') ? 1 : 0);
+  });
+  int right = tokens.fold(0, (val, token) {
+    return val + (token == const SchemeSymbol(')') ? 1 : 0);
+  });
+  return right >= left;
 }
