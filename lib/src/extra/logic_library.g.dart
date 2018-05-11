2dd7b0b066c9a32fe90286b4baf7a5858bda97e4
Jen Thakar <jathak@gmail.com>
2018-05-10 18:54:19 -0700

    Adds query-one to Logic
    query-one works like query, except it finds only one solution.

--- lib/src/extra/logic_library.g.dart
+++ lib/src/extra/logic_library.g.dart
@@ -4,6 +4,7 @@ abstract class _$LogicLibraryMixin {
   void logicStart(Frame env);
   void fact(List<Expression> exprs, Frame env);
   void query(List<Expression> exprs, Frame env);
+  void queryOne(List<Expression> exprs, Frame env);
   void importAll(Frame __env) {
     addPrimitive(__env, const SchemeSymbol('logic'), (__exprs, __env) {
       var __value = undefined;
@@ -28,5 +29,11 @@ abstract class _$LogicLibraryMixin {
     __env.bindings[const SchemeSymbol('?')] =
         __env.bindings[const SchemeSymbol('query')];
     __env.hidden[const SchemeSymbol('?')] = true;
+    addVariableOperandPrimitive(__env, const SchemeSymbol('query-one'),
+        (__exprs, __env) {
+      var __value = undefined;
+      this.queryOne(__exprs, __env);
+      return __value;
+    }, 0, -1);
   }
 }
